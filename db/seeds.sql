1c79f695956e222366135bae8898969be13ea786
winTS <huynhts@gmail.com>
2019-09-05 20:07:47 -0700

    added sql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Jalops n Spicy');
+INSERT INTO burgers (burger_name) VALUES ('Popeyes Spicy Chicken');
