705df0cba363034540d063ef36465aac7df3fd7c
winTS <huynhts@gmail.com>
2019-10-04 19:29:38 -0700

    fixed jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -2,25 +2,17 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "nodeuser",
-  password: "",
-  database: "burgers_db"
-});
+    host: "localhost",
+    user: "nodeuser",
+    password: "",
+    database: "burgers_db"
+  });
+};
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = connection
